6535d182b08341b9b7f55daa669e2a3df5cd9d9a
Kawan02 <kawanng39@gmail.com>
2024-06-16 12:42:41 -0300

    Criando animações no tema

--- aula/tailwind.config.js
+++ aula/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        floatUp: {
+          "0%": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(-8px)" },
+          "100%": { transform: "translatey(0px)" },
+        },
+      },
+      animation: {
+        floatUp: "floatUp 2s infinite",
+      },
       colors: {
         primaria: "#ff00ff",
       },
